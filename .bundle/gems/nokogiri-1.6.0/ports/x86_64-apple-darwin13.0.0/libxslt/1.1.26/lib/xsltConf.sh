dc86028e41f7b15d35dae8aa17684283501de0df
Matias Hoyl <matiashoyl@gmail.com>
2013-12-12 15:34:18 -0300

    Bundle install

---
+++ .bundle/gems/nokogiri-1.6.0/ports/x86_64-apple-darwin13.0.0/libxslt/1.1.26/lib/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/matiashoyl/Ruby/simon-de-cirene/.bundle/gems/nokogiri-1.6.0/ports/x86_64-apple-darwin13.0.0/libxslt/1.1.26/lib"
+XSLT_LIBS="-lxslt  -L/Users/matiashoyl/Ruby/simon-de-cirene/.bundle/gems/nokogiri-1.6.0/ports/x86_64-apple-darwin13.0.0/libxml2/2.8.0/lib -lxml2 -lz -lpthread -liconv -lm "
+XSLT_INCLUDEDIR="-I/Users/matiashoyl/Ruby/simon-de-cirene/.bundle/gems/nokogiri-1.6.0/ports/x86_64-apple-darwin13.0.0/libxslt/1.1.26/include"
+MODULE_VERSION="xslt-1.1.26"
