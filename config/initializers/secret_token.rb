59f2b40271657f0249b36c58b3ffb5e1e4f41e02
Matias Hoyl <matiashoyl@gmail.com>
2013-12-12 12:47:58 -0300

    Bootstrap inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimonDeCirene::Application.config.secret_token = 'bed8ac62706772b3c5844e099088b4bc08af0654f26dc9c17379206a9120258b5e280c707c06372adea5f52349c94bb0d8f54861e943be1874137f7b8f1ea469'
