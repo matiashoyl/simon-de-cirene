097b2838bfd78929bbba09cf679ec80e7d33e1ef
Matias Hoyl <matiashoyl@gmail.com>
2013-12-27 13:37:01 -0300

    Assets nightmare

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require jquery.Rut
 //= require bootstrap-timepicker
 //= require bootstrap-datepicker
+//= require bootstrap
 //= require_self
 //= require_tree .
