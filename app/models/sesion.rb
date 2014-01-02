45454fa31d5cfdbe15ddc5b3bc1665af587f39c7
Matias Hoyl <matiashoyl@gmail.com>
2014-01-02 16:13:58 -0300

    Porcentajes y manejos de situaciones menores a algun porcentaje

--- app/models/sesion.rb
+++ app/models/sesion.rb
@@ -5,4 +5,29 @@ class Sesion < ActiveRecord::Base
  	attr_accessible :comentario, :direccion, :fecha, :hora_inicio, :hora_termino, :tema, :tipo, :curso_id, :user_id
 
  	validates :tema, :tipo, :direccion, :fecha, :hora_inicio, :hora_termino, :curso_id, :user_id, :presence => true
+
+ 	def porcentaje_asistentes
+ 		alumno_sesiones = AlumnoSesion.where(:sesion_id => self).all
+ 		asistentes_presentes = 0.0
+ 		asistentes_totales = 0.0
+ 		alumno_sesiones.each do |alumno_sesion|
+ 			if alumno_sesion.presente
+ 				asistentes_presentes = asistentes_presentes + 1
+ 			end
+ 			asistentes_totales = asistentes_totales + 1
+ 		end
+
+ 		if asistentes_totales > 0
+	 		porcentaje_asistentes = 0
+	 		begin
+	 			porcentaje_asistentes = ((asistentes_presentes/asistentes_totales).round(2)*100).to_i
+	 		rescue
+	 			porcentaje_asistentes = 0
+	 		end
+	 	else
+	 		porcentaje_asistentes = "NA"
+	 	end
+
+ 		return porcentaje_asistentes
+ 	end
 end
