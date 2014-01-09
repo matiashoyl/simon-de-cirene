e76d736457c29dfe8d8b5bb345af7298d13bda09
Matias Hoyl <matiashoyl@gmail.com>
2014-01-09 19:05:24 -0300

    Mejoras en la vista del formulario

--- app/assets/javascripts/formularios.js
+++ app/assets/javascripts/formularios.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
     $("#ss-submit").addClass("btn btn-info");
+    $(".ss-required-asterisk").text("");
+    $(".required-message").text("");
+    $("#ss-submit").parent("td").children("div").text("");
+    $("#ss-form").children("table").addClass("table-striped");
 })
