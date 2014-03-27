12285ff28f3381002b431788cdb18b58be9985b2
Matias Hoyl <matiashoyl@gmail.com>
2014-03-27 12:57:34 -0300

    Se agrega en la vista resumen la asistencia y asistencia sobre el total

--- app/models/curso.rb
+++ app/models/curso.rb
@@ -41,4 +41,45 @@ class Curso < ActiveRecord::Base
 	def relator
 		return User.find(self.relator_jefe_id).name
 	end
+
+	def asistencia
+		sesiones_con_asistencia = 0
+		sumatoria = 0
+		self.sesions.each do |sesion|
+			if sesion.porcentaje_asistentes == "NA"
+				#no hacer nada
+			else
+				sumatoria += sesion.porcentaje_asistentes
+				sesiones_con_asistencia += 1
+			end
+		end
+		if sesiones_con_asistencia != 0 
+			return ((sumatoria/sesiones_con_asistencia).round(2)).to_i
+		else
+			return "NA"
+		end
+	end
+
+	def asistencia_sobre_el_total
+		sesiones = 0
+		sesiones_sin_asistencia = 0
+		sumatoria = 0
+		self.sesions.each do |sesion|
+			if sesion.porcentaje_asistentes == "NA"
+				sesiones_sin_asistencia += 1
+			else
+				sumatoria += sesion.porcentaje_asistentes		
+			end
+			sesiones += 1
+		end
+		if sesiones != 0 
+			if sesiones != sesiones_sin_asistencia
+				return ((sumatoria/sesiones).round(2)).to_i
+			else
+				return "NA"
+			end
+		else
+			return "NA"
+		end
+	end
 end
