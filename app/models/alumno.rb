fcfdd4eb84a6b1d20d46d1f3f3ecfb5be5e528cc
Matias Hoyl <matiashoyl@gmail.com>
2016-09-11 21:14:59 -0300

    Cambios en modelo alumnos

--- app/models/alumno.rb
+++ app/models/alumno.rb
@@ -74,7 +74,7 @@ class Alumno < ActiveRecord::Base
 			case File.extname(file.original_filename)
 			when ".csv" then Roo::Csv.new(file.path, nil, :ignore)
 			when ".xls" then Roo::Excel.new(file.path, packed: false, file_warning: :ignore)
-			when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
+			when ".xlsx" then Roo::Excelx.new(file.path, packed: false, file_warning: :ignore)
 			else raise "Formato de archivo desconocido: #{file.original_filename}"
 			end
 		end
